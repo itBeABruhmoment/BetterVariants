a8e6adcf3e77e3f884e534f9f0c522c647f8cf17
itBeABruhMoment <reee271314@gmail.com>
2022-08-14 17:31:31 -0700

    major refactoring for variants lib

--- jars/BetterVariantsProj/src/better_variants/data/CommonStrings.java
+++ jars/BetterVariantsProj/src/better_variants/data/CommonStrings.java
@@ -5,27 +5,9 @@ import com.fs.starfarer.api.impl.campaign.ids.Personalities;
 
 // stores important strings
 public class CommonStrings {
-    public static final String VARIANT_TAGS_CSV_PATH = "data/bettervariants/variant_tags.csv";
-    public static final String FACTION_TAGS_CSV_PATH = "data/bettervariants/faction_tags.csv";
-    public static final String FLEETS_CSV_PATH = "data/bettervariants/fleets/fleets.csv";
-    public static final String FLEETS_FOLDER_PATH = "data/bettervariants/fleets/";
-    public static final String MOD_ID = "better_variants";
-    public static final String DO_NOT_CHANGE_PERSONALITY_KEY = "$bvNoPersonalityChange";
-    public static final String MODIFIED_IN_BATTLE_KEY = "$bvModifiedInBattle";
-    public static final String FLEET_VARIANT_KEY = "$bvType";
-    public static final HashSet<String> PERSONALITIES =  new HashSet<String>() {{
-        add(Personalities.AGGRESSIVE);  add(Personalities.CAUTIOUS); add(Personalities.RECKLESS);
-        add(Personalities.STEADY);      add(Personalities.TIMID);
-    }};
-    public static final String NEX_VENGANCE_FLEET = "vengeanceFleet";
-    public static final String NEX_INVASION_FLEET = "exerelinInvasionFleet";
-    public static final String NEX_INVASION_SUPPORT_FLEET = "exerelinInvasionSupportFleet";
-    public static final String NEX_SUPPRESSION_FLEET = "nex_suppressionFleet";
-    public static final String NEX_SPECIAL_FORCES_FLEET = "nex_specialForces";
-    public static final String NEX_RELIEF_FLEET = "nex_reliefFleet";
-    public static final String NEX_RESPONSE_FLEET =  "exerelinResponseFleet";
-    public static final String NEX_VULTURE_FLEET =  "nex_vultureFleet";
-    public static final String NEX_MINING_FLEET =  "exerelinMiningFleet";
 
+
+    public static final String MOD_ID = "better_variants";
+    public static final String EXOTIC_BOUNTY_FLEET_TYPE = "bvExoticBounty";
     private CommonStrings() {}
 }
--- jars/BetterVariantsProj/src/better_variants/scripts/BetterVariantsModPlugin.java
+++ jars/BetterVariantsProj/src/better_variants/scripts/BetterVariantsModPlugin.java
@@ -1,27 +1,14 @@
 package better_variants.scripts;
 
-import better_variants.data.FactionData;
-import better_variants.data.VariantData;
 import better_variants.bar_events.BetterVariantsBarEventCreator;
 import better_variants.bar_events.BountyCreationData;
-import better_variants.bar_events.TestEvent;
 import better_variants.data.CommonStrings;
-import better_variants.data.FleetBuildData;
-import better_variants.data.SettingsData;
 
 import java.io.IOException;
-import java.util.List;
-
-import javax.xml.bind.JAXBException;
 
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.campaign.FactionAPI;
 import com.fs.starfarer.api.impl.campaign.intel.bar.events.BarEventManager;
-import com.fs.starfarer.api.impl.campaign.intel.bar.events.BarEventManager.GenericBarEventCreator;
-
-import data.scripts.bounty.MagicBountyData;
-import data.scripts.bounty.MagicBountyData.bountyData;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -36,14 +23,6 @@ public class BetterVariantsModPlugin extends BaseModPlugin {
     @Override
     public void onApplicationLoad() throws IOException, JSONException, Exception
     {
-        log.debug(CommonStrings.MOD_ID + ": loading settings");
-        SettingsData.loadSettings();
-        log.debug(CommonStrings.MOD_ID + ": loading faction data");
-        FactionData.loadData();
-        log.debug(CommonStrings.MOD_ID + ": loading variant data");
-        VariantData.loadData();
-        log.debug(CommonStrings.MOD_ID + ": loading fleet build data");
-        FleetBuildData.loadData();
         log.debug(CommonStrings.MOD_ID + ": loading bounty data");
         BountyCreationData.loadData();
     }
@@ -51,10 +30,6 @@ public class BetterVariantsModPlugin extends BaseModPlugin {
     @Override
     public void onGameLoad(boolean newGame)
     {
-        log.debug(CommonStrings.MOD_ID + ": adding listener");
-        Global.getSector().addTransientListener(new BetterVariantsListener(false));
-        log.debug(CommonStrings.MOD_ID + ": initializing faction aggression values");
-        UnofficeredPersonalitySetPlugin.innitDefaultAggressionValues();
         log.debug(CommonStrings.MOD_ID + ": adding bar event manager");
         BarEventManager barEventManager = BarEventManager.getInstance();
 
