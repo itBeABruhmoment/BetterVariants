9cb15d00cfdc74eb76bbad028d5f122550a9dfee
itBeABruhMoment <reee271314@gmail.com>
2023-05-28 10:43:32 -0700

    more variants

--- data/variants/list_variants.py
+++ data/variants/list_variants.py
@@ -5,7 +5,9 @@ ls = []
 
 for path, currentDirectory, files in os.walk("."):
     for file in files:
-    	ls.append(file.split(".")[0])
+    	name = file.split(".")
+    	if name[1] == 'variant':
+    		ls.append(name[0])
 ls.sort()
 for str in ls:
 	print(str)
