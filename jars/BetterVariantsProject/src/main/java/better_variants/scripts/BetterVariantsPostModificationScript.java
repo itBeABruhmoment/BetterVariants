a44ad507ee3fd5c2d78f3187936f28c23cf607e5
itBeABruhMoment <reee271314@gmail.com>
2023-06-02 21:40:33 -0700

    fixed officer checking

--- jars/BetterVariantsProject/src/main/java/better_variants/scripts/BetterVariantsPostModificationScript.java
+++ jars/BetterVariantsProject/src/main/java/better_variants/scripts/BetterVariantsPostModificationScript.java
@@ -79,7 +79,7 @@ public class BetterVariantsPostModificationScript implements FleetEditingScript 
         final OfficerFactory officerFactory = new OfficerFactory();
         for(final FleetMemberAPI memberAPI : fleet.getMembersWithFightersCopy()) {
             final PersonAPI officer = memberAPI.getCaptain();
-            if(officer != null && officer.getStats().getLevel() != 0) {
+            if(Util.isOfficer(officer)) {
                 final String variant = memberAPI.getVariant().getOriginalVariant();
                 OfficerFactoryParams officerFactoryParams = new OfficerFactoryParams(
                         variant,
